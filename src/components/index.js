8769fdc456e2e106363767ecc2e2575e7fa6d8cf
Dmarcotrigiano <79153083+Dmarcotrigiano@users.noreply.github.com>
2021-03-16 12:37:28 -0400

    lazy commit
    Definitely need to be better about committing my work as I do it.

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from './NavigationBar/Navbar';
+export { default as Products } from './ProductPage/ProductPage';
+export { default as Cart } from './Cart/Cart';
+export { default as Checkout } from './Checkout/Checkout/Checkout';
